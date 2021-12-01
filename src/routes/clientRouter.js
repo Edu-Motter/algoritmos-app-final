eac1aaf6761a23b2b20ae73aa1899998ee0c33e8
Edu-Motter <motter.edu@gmail.com>
2021-12-01 20:59:28 -0300

    retirando auth

--- src/routes/clientRouter.js
+++ src/routes/clientRouter.js
@@ -4,11 +4,11 @@ const clientController = require("../controllers/clientController");
 const auth = require("../middlewares/auth");
 const validation = require("../middlewares/validators/clientValidator");
 
-clientRouter.get("/listAllClients", auth, validation, clientController.listAllClients);
-clientRouter.get("/listClientByCnpj", auth, validation, clientController.searchClientByCnpj);
-clientRouter.get("/listClientById", auth, validation, clientController.searchClientById);
-clientRouter.delete("/deleteClient", auth, validation, clientController.deleteClient);
-clientRouter.put("/updateClient", auth, validation, clientController.updateClient)
-clientRouter.post("/newClient", auth, validation, clientController.newClient);
+clientRouter.get("/listAllClients",  validation, clientController.listAllClients);
+clientRouter.get("/listClientByCnpj",  validation, clientController.searchClientByCnpj);
+clientRouter.get("/listClientById",  validation, clientController.searchClientById);
+clientRouter.delete("/deleteClient",  validation, clientController.deleteClient);
+clientRouter.put("/updateClient",  validation, clientController.updateClient)
+clientRouter.post("/newClient",  validation, clientController.newClient);
 
 module.exports = clientRouter;
