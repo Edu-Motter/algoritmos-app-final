d07fe8f06f288e16b14f6b9d3709405997da94b6
Edu-Motter <motter.edu@gmail.com>
2021-12-02 00:31:51 -0300

    hash nas senhas

--- src/database/seeders/20211124021848-delivery_men.js
+++ src/database/seeders/20211124021848-delivery_men.js
@@ -9,28 +9,28 @@ module.exports = {
           associateId: 1,
           name: "Deliveryman Test",
           cpf: "11111111156",
-          password: "teste@123",
+          password: "$2a$12$ImHG0RphH01UK80moIFnyO1J217waiaFoELKh0TldySCNjOpWM76G",
           phone: "(41) 99999-9999",
         },
         {
           associateId: 1,
           name: "Deliveryman Test 2",
           cpf: "11154111157",
-          password: "teste@123",
+          password: "$2a$12$ImHG0RphH01UK80moIFnyO1J217waiaFoELKh0TldySCNjOpWM76G",
           phone: "(41) 99999-9999",
         },
         {
           associateId: 2,
           name: "Deliveryman Test 3 ",
           cpf: "11132111158",
-          password: "teste@123",
+          password: "$2a$12$ImHG0RphH01UK80moIFnyO1J217waiaFoELKh0TldySCNjOpWM76G",
           phone: "(41) 99999-9999",
         },
         {
           associateId: 2,
           name: "Deliveryman Test 4",
           cpf: "11112111159",
-          password: "teste@123",
+          password: "$2a$12$ImHG0RphH01UK80moIFnyO1J217waiaFoELKh0TldySCNjOpWM76G",
           phone: "(41) 99999-9999",
         }
       ]
