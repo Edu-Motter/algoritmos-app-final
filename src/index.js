bb068e56db98b19d69f04a15151f08c57f1cf776
Erick <erickrg149@gmail.com>
2021-12-12 22:13:42 -0300

    make it better

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const router = require("./routes/router");
 require("./database")
+const server = require('http').Server(app);
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true}));
