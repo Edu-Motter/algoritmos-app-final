f2497c5ca8bb610771d136ab65ce2a355c768f8c
Erick <erickrg149@gmail.com>
2021-12-13 18:39:25 -0300

    make it better

--- src/middlewares/auth.js
+++ src/middlewares/auth.js
@@ -1,7 +1,7 @@
 const jwt = require("jsonwebtoken");
 
 function verifyJWT(req, res, next) {
-    const token = req.headers["x-access-token"];
+    const token = req.headers["Authorization"];
     console.log(token);
     if (!token) return res.status(401).json({msg: "Token não foi definido"});
 
