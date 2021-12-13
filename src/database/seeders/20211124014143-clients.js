22abbd3e8715e88c2796ccd91f2b5f83d164229a
Erick <erickrg149@gmail.com>
2021-12-12 21:27:23 -0300

    aaaaa

--- src/database/seeders/20211124014143-clients.js
+++ src/database/seeders/20211124014143-clients.js
@@ -6,25 +6,25 @@ module.exports = {
       "Clients",
       [
         {
-          associateId: 1,
+          associateId: 4,
           companyName: "Clinte Bom",
           cnpj: "03726350000121",
           address: "Address Test, 234",
         },
         {
-          associateId: 1,
+          associateId: 4,
           companyName: "Client Ruim 2",
           cnpj: "03726350000122",
           address: "Address Test, 233",
         },
         {
-          associateId: 1,
+          associateId: 4,
           companyName: "Client Mediano 3",
           cnpj: "03726350000133",
           address: "Address Test, 231",
         },
         {
-          associateId: 1,
+          associateId: 14,
           companyName: "Client Otimo 4",
           cnpj: "03726350000134",
           address: "Address Test, 232",
